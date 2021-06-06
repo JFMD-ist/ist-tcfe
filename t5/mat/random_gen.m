a3f248293b0b35ff60eadf7d12673f586445f6ca
João Domingos <joaomdomingos@gmail.com>
2021-06-06 13:36:22 +0100

    Changed the architecture of the circuit which was working so well...

--- t5/mat/random_gen.m
+++ t5/mat/random_gen.m
@@ -10,7 +10,6 @@ format long
 nR1 = input("Number of 1k resistors = ");
 nR2 = input("Number of 10k resistors = ");
 nR3 = input("Number of 100k resistors = ");
-cost = (3-nR1)*1+(3-nR2)*10+(3-nR3)*100;
 
 n = round((nR1+nR2+nR3-1)*rand(1))+1; 
 n1 = round(nR1*rand(1));  %the numbers inside the round function are the number of resistors of that type which are available (in this case 3 of 1k, 3 of 10k, and 3 of 100k)
@@ -57,4 +56,52 @@ disp(rn2)
 disp(rn3)
 disp(R)
 
+%-------------------------------------------------------------------------
+%Capacitors
+%-------------------------------------------------------------------------
 
+%Rt is the value which we want to get out of this
+nC1 = input("Number of 220n capacitors = ");
+nC2 = input("Number of 1u capacitors = ");
+
+n = round((nC1+nC2-1)*rand(1))+1; 
+n1 = round(nC1*rand(1));  %the numbers inside the round function are the number of resistors of that type which are available (in this case 3 of 1k, 3 of 10k, and 3 of 100k)
+n2 = round(nC2*rand(1));
+
+cn1 = [];
+cn2 = [];
+for i=1:1:n
+	cn1 = horzcat(cn1, 0);
+	cn2 = horzcat(cn2, 0);
+endfor
+
+if(n1>0)
+	for i=1:1:n1
+		a = round((n-1)*rand(1))+1;
+		cn1(a) += 1;
+	endfor
+endif
+if(n2>0)
+	for i=1:1:n2
+		a = round((n-1)*rand(1))+1;
+		cn2(a) += 1;
+	endfor
+endif
+
+Cv = [];
+G = 0;
+for i=1:1:n
+	Cv = horzcat(Cv, cn1(i)*220 + cn2(i)*1000);
+endfor
+for i=1:1:n
+	if(Cv(i))
+		G += 1/(Cv(i));
+	endif
+endfor
+if(G>0)
+	C = 1/G;
+endif
+
+disp(cn1)
+disp(cn2)
+disp(C)
--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -11,23 +11,25 @@ Zin = 1e6;
 Zout = 50;
 Av = 1e5;
 
-R1 = 2000+1/(1/1000 + 2/100000);
-C1 = 1/(1/220e-9 + 1/1e-6);
+R1 = 1250;
+C1 = 3000e-9;
+R2 = 50e3;
+C2 = 220e-9;
 Z1 = R1 + 1/(C1*i*w);
 Z2 = 120e3;
-Rl = 8;
+Zl = 1/(C2*i*w);
 
 %-------------------------------------------------------------------------
 %Mesh Analysis
 %-------------------------------------------------------------------------
 
-M = [[Z1-Zin, 0, -Zin];[Av*Zin, Zout+Rl, Zout+Av*Zin];[Av*Zin-Zin, Zout, Z2+Zout+Av*Zin-Zin]];
+M = [[Z1-Zin, 0, -Zin];[Av*Zin, Zout+Zl, Zout+Av*Zin];[Av*Zin-Zin, Zout, Z2+Zout+Av*Zin-Zin]];
 
 B = [Vin; 0; 0];
 
 I = M\B;
 
-Vout = Rl*I(2);
+Vout = Zl*I(2);
 
 %-------------------------------------------------------------------------
 %Central Frequency Analysis
@@ -48,18 +50,46 @@ gain_mag = [];
 gain_ph = [];
 for j=1:1:size(f, 2)
 	w = 2*pi*f(j);
-	gain_mag = horzcat(gain_mag, abs(eval(Vout)/Vin));
+	gain_mag = horzcat(gain_mag, 20*log10(abs(eval(Vout)/Vin)));
 	bruh = arg(eval(Vout)/Vin)*180/pi;
 	gain_ph = horzcat(gain_ph, bruh);
 endfor
 
+gain_max = gain_mag(1, 1);
+for j=1:1:size(gain_mag, 2)
+	if gain_mag(1, j) > gain_max
+		gain_max = gain_mag(1, j);
+	endif
+endfor
+
+cutoff = gain_max - 3;
+cut = [];
+for j=1:1:size(gain_mag, 2)
+cut = horzcat(cut, cutoff);
+endfor
+
+cf = [];
+for j=1:1:size(gain_mag, 2)
+	if abs(gain_mag(1, j) - cutoff) < 0.6
+		cf = horzcat(cf, f(1, j));
+	endif
+endfor
+
+f_central = sqrt(cf(1, 1) * cf(1, 2))
+freq_dev = abs(f_central - 1000)
+gain_dev = abs(gain - 40)
+cost = 13322.9920387+324.228
+merit = 1/(cost*(gain_dev + freq_dev + 1e-6))
+
 hf = figure();
 semilogx(f, gain_mag);
+hold on;
+semilogx(f, cut);
 grid on;
 xlabel ("frequency (Hz)");
 ylabel ("Magnitude");
 title ("Magnitude plot");
-legend("vo/vi(f)","location", "northwestoutside");
+legend("vo/vi(f)", "cutoff", "location", "northwestoutside");
 saveas(hf, "gain_mag.pdf");
 
 hg = figure();
