eb7daa8a859f5aaa6ee3f410acf50062bc77c2d9
João Domingos <joaomdomingos@gmail.com>
2021-06-06 16:52:01 +0100

    Changed circuit architecture again... how fun

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -7,29 +7,28 @@ syms w
 %Incremental Model Specs
 
 Vin = 10e-3;
-Zin = 1e6;
-Zout = 50;
-Av = 1e5;
+Zin = 10e6;
+Zout = 10;
+Av = 1e4;
 
-R1 = 1250;
-C1 = 3000e-9;
-R2 = 50e3;
+R1 = 1000;
+C1 = 2220e-9;
+Zc1 = 1/(C1*i*w);
+R2 = 103.7233188894238e3;
 C2 = 220e-9;
-Z1 = R1 + 1/(C1*i*w);
-Z2 = 120e3;
-Zl = 1/(C2*i*w);
+Zc2 = 1/(C2*i*w);
 
 %-------------------------------------------------------------------------
 %Mesh Analysis
 %-------------------------------------------------------------------------
 
-M = [[Z1-Zin, 0, -Zin];[Av*Zin, Zout+Zl, Zout+Av*Zin];[Av*Zin-Zin, Zout, Z2+Zout+Av*Zin-Zin]];
+M = [[Zc1+R1, -R1, 0, 0];[-R1, R1+Zin, Zin, 0];[0, Zin+Av*Zin, Zin+Av*Zin+Zout+R2, Zout];[0, Av*Zin, Av*Zin+Zout, Zout+Zc2]];
 
-B = [Vin; 0; 0];
+B = [Vin; 0; 0; 0];
 
 I = M\B;
 
-Vout = Zl*I(2);
+Vout = Zc2*I(4);
 
 %-------------------------------------------------------------------------
 %Central Frequency Analysis
@@ -38,7 +37,7 @@ Vout = Zl*I(2);
 w = 2*pi*1000;
 Vout_mag = abs(Vout);
 input_imp = Vin/eval(I(1))
-output_imp = eval(Vout)/eval(I(2))
+output_imp = eval(Vout)/eval(I(4))
 gain = eval(Vout_mag)/Vin
 
 %-------------------------------------------------------------------------
@@ -70,12 +69,14 @@ endfor
 
 cf = [];
 for j=1:1:size(gain_mag, 2)
-	if abs(gain_mag(1, j) - cutoff) < 0.6
+	if abs(gain_mag(1, j) - cutoff) < 0.5
 		cf = horzcat(cf, f(1, j));
 	endif
 endfor
 
-f_central = sqrt(cf(1, 1) * cf(1, 2))
+f_low = cf(1, 1)
+f_high = cf(1, end)
+f_central = sqrt(f_low*f_high)
 freq_dev = abs(f_central - 1000)
 gain_dev = abs(gain - 40)
 cost = 13322.9920387+324.228
