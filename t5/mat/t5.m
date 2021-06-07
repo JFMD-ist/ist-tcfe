099ed0278d4ddbe256af34d580e37a8c03e6de1c
João Domingos <joaomdomingos@gmail.com>
2021-06-07 23:23:59 +0100

    Another happy landing

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -11,11 +11,11 @@ Zin = 5e6;
 Zout = 50;
 Av = 1e5;
 
-R1 = 30e3;
+R1 = 10e3;
 R2 = 1e3;
-R3 = 100e3;
-R4 = 2e3;
-C1 = 2440e-9;
+R3 = 150e3;
+R4 = 500;
+C1 = 265.0602409638554e-9;
 Zc1 = 1/(C1*i*w);
 C2 = 220e-9;
 Zc2 = 1/(C2*i*w);
@@ -41,6 +41,17 @@ Vout_mag = abs(Vout);
 input_imp = Vin/(1000*eval(I(1)))
 output_imp = eval(Vout)/(eval(I(4))*1000)
 gain = eval(Vout_mag)/Vin
+
+table_end = '\\ \hline';
+filename = "central_freq.tex";
+fid = fopen(filename, "w");
+fprintf(fid, "\\begin{tabular}{|c|c|}\n");
+fprintf(fid, "\\hline\n");
+fprintf(fid, "Input Impedance in $k\\Omega$ & %f%+fj %s\n", real(input_imp), imag(input_imp), table_end);
+fprintf(fid, "Output Impedance in $k\\Omega$ & %f%+fj %s\n", real(output_imp), imag(output_imp), table_end);
+fprintf(fid, "Gain (dB)  & %f %s\n", 20*log10(gain), table_end);
+fprintf(fid, "\\end{tabular}\n");
+fclose(fid);
 
 %-------------------------------------------------------------------------
 %Frequency response
@@ -81,8 +92,33 @@ f_high = cf(1, end)
 f_central = sqrt(f_low*f_high)
 freq_dev = abs(f_central - 1000)
 gain_dev = abs(gain - 100)
-cost = 13323.2920387 + 135.66
+cost = 13323.2920387 + 316.66
 merit = 1/(cost*(gain_dev + freq_dev + 1e-6))
+
+filename = "freq_response.tex";
+fid = fopen(filename, "w");
+fprintf(fid, "\\begin{tabular}{|c|c|}\n");
+fprintf(fid, "\\hline\n");
+fprintf(fid, "Lower Cutoff Frequency & %f %s\n", f_low, table_end);
+fprintf(fid, "Central Frequency & %f %s\n", f_central, table_end);
+fprintf(fid, "Upper Cutoff Frequency  & %f %s\n", f_high, table_end);
+fprintf(fid, "Merit  & %e %s\n", merit, table_end);
+fprintf(fid, "\\end{tabular}\n");
+fclose(fid);
+
+filename = "results.tex";
+fid = fopen(filename, "w");
+fprintf(fid, "\\begin{tabular}{|c|c|}\n");
+fprintf(fid, "\\hline\n");
+fprintf(fid, "Input Impedance in $k\\Omega$ & %f%+fj %s\n", real(input_imp), imag(input_imp), table_end);
+fprintf(fid, "Output Impedance in $k\\Omega$ & %f%+fj %s\n", real(output_imp), imag(output_imp), table_end);
+fprintf(fid, "Gain (dB)  & %f %s\n", 20*log10(gain), table_end);
+fprintf(fid, "Lower Cutoff Frequency & %f %s\n", f_low, table_end);
+fprintf(fid, "Central Frequency & %f %s\n", f_central, table_end);
+fprintf(fid, "Upper Cutoff Frequency  & %f %s\n", f_high, table_end);
+fprintf(fid, "Merit  & %e %s\n", merit, table_end);
+fprintf(fid, "\\end{tabular}\n");
+fclose(fid);
 
 hf = figure();
 semilogx(f, gain_mag);
